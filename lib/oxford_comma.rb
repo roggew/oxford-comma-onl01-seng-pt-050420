6f3a9b4300fb677ee2b314e7f5ecc4da1c1dd2e3
William Rogge <williamroggeiv@gmail.com>
2020-05-11 22:30:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
   array.join ( " and ")
   
 else array.length > 3
-  array.join(", ")
-  array.reverse.sub("," , "and").reverse
+  array.join(", ") &&
+  array[-1].insert(-2, "and")
 end
 end
