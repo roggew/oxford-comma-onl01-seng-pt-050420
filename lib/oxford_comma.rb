828e5edf0d56410d1d7c9e5485838c5ec0a73f25
William Rogge <williamroggeiv@gmail.com>
2020-05-11 21:43:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,6 @@ def oxford_comma(array)
   return array.join ( " and ")
   
 else array.length > 3
-  return array.join ("," "and" << index -1-1)
+  return array.join ("," "and" << index = -1-1)
 end
 end
